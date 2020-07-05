be5c0d1bab55c88d408ff235cd8e34748634398c
Fawad <fawad_12@outlook.com>
2020-07-05 19:47:35 +0500

    useState with arrays

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HookCounterOne from './components/usestate/HookCounterOne';
 import HookCounterTwo from './components/usestate/HookCounterTwo';
 import HookObjectThree from './components/usestate/HookObjectThree';
+import HookCouterFour from './components/usestate/HookCouterFour';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HookCounterOne />
       <HookCounterTwo />
       <HookObjectThree />
+      <HookCouterFour />
     </div>
   );
 }
