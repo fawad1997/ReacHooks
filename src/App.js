c1eea87723255e3aa489408aeacbb9c3921d5eb3
Fawad <fawad_12@outlook.com>
2020-07-05 19:58:01 +0500

    siimpleUseEffect that renders after every update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import HookCounterOne from './components/usestate/HookCounterOne';
-import HookCounterTwo from './components/usestate/HookCounterTwo';
-import HookObjectThree from './components/usestate/HookObjectThree';
-import HookCouterFour from './components/usestate/HookCouterFour';
+import EffectCounterOne from './components/useEffect/EffectCounterOne';
+
+//   import HookCounterOne from './components/usestate/HookCounterOne';
+// import HookCounterTwo from './components/usestate/HookCounterTwo';
+// import HookObjectThree from './components/usestate/HookObjectThree';
+// import HookCouterFour from './components/usestate/HookCouterFour';
+
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
-      <HookCounterTwo />
-      <HookObjectThree />
-      <HookCouterFour />
+      <EffectCounterOne />
     </div>
   );
 }
