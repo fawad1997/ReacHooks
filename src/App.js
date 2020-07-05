4b305a99ea0c620c17922e535f91cdbfde28e5d3
Fawad <fawad_12@outlook.com>
2020-07-06 01:04:30 +0500

    bootstrap and react router install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import DataFetching from './components/fetchdata/DataFetching';
+// import DataFetching from './components/fetchdata/DataFetching';
 // import EffectCounterOne from './components/useEffect/EffectCounterOne';
 // import EffectOnceTwo from './components/useEffect/EffectOnceTwo';
 // import EffectIntervalCounter from './components/useEffect/EffectIntervalCounter';
@@ -14,7 +14,7 @@ import DataFetching from './components/fetchdata/DataFetching';
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+     
     </div>
   );
 }
