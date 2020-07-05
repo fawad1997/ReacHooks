7887e7e6bb171a8808ea3fb489d4e984fa46002d
Fawad <fawad_12@outlook.com>
2020-07-05 19:29:22 +0500

    useState with previous state example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HookCounterOne from './components/usestate/HookCounterOne';
+import HookCounterTwo from './components/usestate/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
      <HookCounterOne />
+     <HookCounterTwo />
     </div>
   );
 }
